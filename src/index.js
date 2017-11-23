8c518c93067c8d6b3dbbd67cbcc0241869b87eae
Lupino <lmjubuntu@gmail.com>
2017-11-23 17:24:47 +0800

    fixed. import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 export {weixinAuth, User} from './user';
 export {fetch, fetchJSON, promiseToCallback, callbackToPromise} from './utils';
+import {weixinAuth, User} from './user';
+import {fetch, fetchJSON, promiseToCallback, callbackToPromise} from './utils';
 
 if (typeof window !== 'undefined') {
   window['sdk'] = {
